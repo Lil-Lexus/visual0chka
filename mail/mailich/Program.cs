74bb89924a4fcd0fbcfd530a75399fe22af5d4bb
Lil-Lexus <57208908+Lil-Lexus@users.noreply.github.com>
2020-05-23 23:35:21 +0300

    Add files via upload

---
+++ mail/mailich/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Mail;
+using System.Net;
+
+namespace mail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите свою почту");
+            string p = Console.ReadLine();
+            Console.WriteLine("Ваше имя");
+            string n = Console.ReadLine();
+            MailAddress from = new MailAddress(p, n);
+
+            Console.WriteLine("Введите почту получателя");
+            string m = Console.ReadLine();
+            MailAddress to = new MailAddress(m);
+
+
+            using (MailMessage k = new MailMessage(from, to))
+            using (SmtpClient smtp = new SmtpClient())
+            {
+                Console.WriteLine("Введите тему сообщения");
+                k.Subject = Console.ReadLine();
+                Console.WriteLine("Введите текст сообщения");
+                k.Body = Console.ReadLine();
+                k.IsBodyHtml = true;
+
+                smtp.Host = "smtp.yandex.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+
+                Console.WriteLine("Пароль от вашей почты");
+                string b = Console.ReadLine();
+                smtp.Credentials = new NetworkCredential(p, b);
+                smtp.Send(k);
+            }
+            Console.Read();
+        }
+    }
+}
